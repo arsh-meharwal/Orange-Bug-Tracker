bf3c203a51ff6e5085efe5fd868957056d3632d0
arsh-meharwal <arshmeharwal98@gmail.com>
2024-05-16 04:39:40 -0500

    Updated Minor Bugs

--- services/common.js
+++ services/common.js
@@ -9,13 +9,13 @@ exports.sanitizeUser = (user) => {
 };
 
 exports.cookieExtractor = function (req) {
-  let token = null;
-  if (req && req.cookies) {
-    token = req.cookies["jwt"];
-  }
+  // let token = null;
+  // if (req && req.cookies) {
+  //   token = req.cookies["jwt"];
+  // }
   //random token for test
-  // token =
-  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZjYwOWY0MDUyYmI3M2IyYzhiMjY5ZSIsInJvbGUiOiJBZG1pbiIsImlhdCI6MTcxNTMzMTI2M30.Gv53IpSIZPf-DRhNgsD8Ae1a_smbhGRH3tV16tvgLTc";
+  token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZjYwOWY0MDUyYmI3M2IyYzhiMjY5ZSIsInJvbGUiOiJBZG1pbiIsImlhdCI6MTcxNTMzMTI2M30.Gv53IpSIZPf-DRhNgsD8Ae1a_smbhGRH3tV16tvgLTc";
 
   return token;
 };
